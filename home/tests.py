c6b457dd62cd0c890e1385429ccc0248bbb2b80e
Buddy Lindsey <blindsey@summitesp.com>
2017-08-15 15:35:54 -0700

    Add ViewSet Test

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
-# Create your tests here.
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+from rest_framework import mixins
+from django_filters import rest_framework as filters
+
+from .views import CoinViewSet
+
+from .filters import BitFilter, CoinFilter
+from .serializers import CoinSerializer
+
+
+class CoinViewSetTest(SimpleTestCase):
+    def setUp(self):
+        self.view = CoinViewSet()
+
+    def test_attrs(self):
+        self.assertIsInstance(self.view, mixins.CreateModelMixin)
+        self.assertIsInstance(self.view, mixins.ListModelMixin)
+        self.assertIsInstance(self.view, mixins.UpdateModelMixin)
+        self.assertIsInstance(self.view, mixins.RetrieveModelMixin)
+        self.assertIsInstance(self.view, mixins.DestroyModelMixin)
+        self.assertIsInstance(self.view, viewsets.GenericViewSet)
+
+        self.assertEqual(self.view.permission_classes, (IsAuthenticated,))
+        self.assertEqual(self.view.serializer_class, CoinSerializer)
+        self.assertSequenceEqual(
+            self.view.filter_backends,
+            (filters.DjangoFilterBackend, BitFilter))
+
+        self.assertEqual(self.view.filter_class, CoinFilter)
